220b148d2cb68ba6daf4ab5a292d066e94e0f385
Hector Corea <corea331@gmail.com>
2026-01-06 11:17:54 -0300

    estilos

--- src/components/layout/globalLayout.jsx
+++ src/components/layout/globalLayout.jsx
@@ -7,22 +7,43 @@ const Layout = ({ children }) => {
 
   return (
     <AppShell
-      header={{ height: 'auto' }}
-      footer={{ height: 'auto' }}
+      header={{ height: { base: 100, sm: 120 } }}
+      footer={{ height: { base: 100, sm: 120 } }}
       padding={0}
     >
       {/* Header con estilos del CSS */}
-      <AppShell.Header className="mantine-header">
+      <AppShell.Header 
+        className="mantine-header"
+        withBorder={false}
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1000,
+        }}
+      >
         <Header />
       </AppShell.Header>
 
       {/* Contenido principal */}
-      <AppShell.Main className="layout-main">
+      <AppShell.Main 
+        className="layout-main"
+        style={{
+          minHeight: 'calc(100vh - 260px)', // 120px header + 140px footer
+          overflowY: 'auto',
+        }}
+      >
         {children}
       </AppShell.Main>
 
       {/* Footer con estilos del CSS */}
-      <AppShell.Footer className="mantine-footer">
+      <AppShell.Footer
+        className="mantine-footer"
+        withBorder={false}
+        style={{
+          position: 'relative',
+          bottom: 0,
+        }}
+      >
         <Footer />
       </AppShell.Footer>
     </AppShell>
